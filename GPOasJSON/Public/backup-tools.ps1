8bbf196f5f6f0a004d4e481e94fdb1b4ad7e4745
kevkv <kevin.kvissberg@gmail.com>
2024-03-28 00:06:23 +0100

    Backup plus other fixes

---
+++ GPOasJSON/Public/backup-tools.ps1
@@ -0,0 +1,45 @@
+Function New-GPRegistyBackup {
+    param (
+        $gpoName,
+        $backupPath = ".\Data"
+    )
+    $gpo = [xml](Get-GPOReport -ReportType Xml -Name $gpoName)
+    $gpoData = @{
+        Name = $gpoName
+    }
+    if ($null -ne $gpo.gpo.Computer.ExtensionData) {
+        $gpoData += @{ComputerConfiguration = @{Registry = @()}}
+        foreach ($key in $gpo.gpo.Computer.ExtensionData.Extension.RegistrySettings.Registry.Properties) {
+            $registryHashTable = @{
+                Key = "$($key.hive)\\$($key.key)"
+                ValueName = $key.name
+                Value = $key.value
+                Type = $key.type
+                Action = $key.action
+                Context = "Computer"
+            }
+            $gpoData.ComputerConfiguration.Registry += $registryHashTable
+        }
+    }
+    if ($null -ne $gpo.gpo.User.ExtensionData) {
+        $gpoData += @{UserConfiguration = @{Registry = @()}}
+        foreach ($key in $gpo.gpo.User.ExtensionData.Extension.RegistrySettings.Registry.Properties) {
+            $registryHashTable = @{
+                Key = "$($key.hive)\\$($key.key)"
+                ValueName = $key.name
+                Value = $key.value
+                Type = $key.type
+                Action = $key.action
+                Context = "User"
+            }
+            $gpoData.UserConfiguration.Registry += $registryHashTable
+        }
+    }
+    
+    if (!(Test-Path $backupPath)) {
+        New-Item -Path $backupPath -ItemType Directory
+    }
+
+    $gpoData | ConvertTo-Json -Depth 100 | Format-Json | Out-File "$backupPath\$gpoName.json"
+}
+new-GPRegistyBackup "anotherGPO"
