63acc1012dafac95043de70a727f92e89c20b0f8
kevkv <kevin.kvissberg@gmail.com>
2024-05-09 21:01:05 +0200

    staged comit

--- GPOasJSON/Public/creator-tools.ps1
+++ GPOasJSON/Public/creator-tools.ps1
@@ -62,34 +62,29 @@ function Set-GPRegistyValuesPlus {
     }
     Write-Output "Completed updating GPOs with registry values."
 }
-# SIG # Begin signature block
-# MIIFWwYJKoZIhvcNAQcCoIIFTDCCBUgCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUlMERA2UyuTroLLL9uj6iG1Lr
-# WYWgggL2MIIC8jCCAdqgAwIBAgIQYseIafgwqaZNeZ4d0CYhxTANBgkqhkiG9w0B
-# AQsFADAaMRgwFgYDVQQDEw9LZXZpbiBLdmlzc2JlcmcwHhcNMjMxMjE4MjMxODA0
-# WhcNMjQxMjE4MDUxODA0WjAaMRgwFgYDVQQDEw9LZXZpbiBLdmlzc2JlcmcwggEi
-# MA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDKlNjwOPiKcBJ86HgpMe6dojbh
-# 8stpdYz+D9rLchQwSVRLJkay4McHeETdeUTRPgqVZfnt3ExwJpQoEwKw3mKcU+xe
-# OQfODBAURPneH6AU774u2es3/qfIWITlW423s4DInw4X1LC4wblD59fnprfF1MpA
-# ZvouL+NndDlDjf8JRvHL3XgaD9niT2g2+VeLTkZ3F1VYT5itH1S2a5jwmkesniOz
-# CyEBwpF/J/UlTcID2Cgue7gnbxPaBPxphqvHGahufKGIwXutO7++6pgV0no7ZMFh
-# wrXudFf2JUUGR2HhiB69RU+5FAlNngyDfzqRSVBQo0q8PX2nA0Er51yuuYepAgMB
-# AAGjNDAyMAwGA1UdEwEB/wQCMAAwIgYDVR0lAQH/BBgwFgYIKwYBBQUHAwMGCisG
-# AQQBgjdUAwEwDQYJKoZIhvcNAQELBQADggEBADcCLTlOYo8cRrcuhqDTvvc+7u7E
-# +epENHrEXm3lNNgDZZDlYhgj3M5+Oewl6mSiE6RB9YoPwpZ4Xc7nmOQD2bZhELfP
-# Zqy0NQ5yXHQ6frFeJ0FGr/XL3wTlvpaknfCxX7YcnLzw6e3I2psbSfOUA6+JL9T8
-# tx7GZsWyKXmkncw8P7WzLHPEuVGnaOaUPs8HozzWlwNXoawXo5RwaCg/AGTsiiAH
-# DreH/1myE+vbPFeAQyTlDhf8wVigXRuVWALn0YqaUG2yIoaOteqZdGc+vGL9JFpx
-# nqgP7LwSgVnt9wNdO+9LwCgFyzvtVwSEojSaC0ymHB3rlL3X5tna3O4K1TMxggHP
-# MIIBywIBATAuMBoxGDAWBgNVBAMTD0tldmluIEt2aXNzYmVyZwIQYseIafgwqaZN
-# eZ4d0CYhxTAJBgUrDgMCGgUAoHgwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZ
-# BgkqhkiG9w0BCQMxDAYKKwYBBAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYB
-# BAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQUU97cjW4hcVGO+adeYKRJUNj0qGkwDQYJ
-# KoZIhvcNAQEBBQAEggEAO2rugX3C5SaNtwR+qwqwcM+pGLWN1vaRkpDbordywEpX
-# MD2hNCwKwp81JecXr/fNvCx8FYbrgFxeCeO8oIgSr11jdoMj0tsdehNllQaYBUDr
-# P6PlTOAyUmrZbbphxESf/V1emUr//Y6h9WcelgQZpBkm12ajxgAFNhGNHRoFxSfF
-# KAusSjEr+8WfGF2gfcW+A2A6eZKMspV6me81kAFush8cxRRaEf7aetJNYscZ3Qvb
-# RZNokB7Zl3LLqQrPk5jYMDRtHIvglfsox837hiwqUHbSMmoWa+4p2UItfpEAKWob
-# lg/ajtV0NXK2VO3b8phDYO7s/2Sc6/35leKbZTw2/Q==
-# SIG # End signature block
+
+function Restore-OUStructure {
+    param (
+        $backupFile = "$PSScriptRoot\..\Data\ou.json",
+        [switch]$restoreGPOLinks
+    )
+    $ouData = Get-Content $backupFile | ConvertFrom-Json
+
+    $domainName = (Get-ADDomain).Name
+    $domainRoot = (Get-ADDomain).DNSRoot
+    $domainDistinguishedName = (Get-ADDomain).DistinguishedName
+
+    function Create-OU {
+        param (
+            $name,
+            $dn
+        )
+        $ouPath = "OU=$name,$dn"
+        if (-Not (Get-ADOrganizationalUnit -Filter {Name -eq $name})) {
+            New-ADOrganizationalUnit -Name $name -Path $dn
+        }
+    }
+    foreach ($ou in $ouData) {
+        $ou.values
+    }
+}
